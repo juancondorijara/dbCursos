59148e8001f67f0846c8a3dec6e283dbb89f11d1
juancondorijara <Juan.condori.jara@vallegrande.edu.pe>
2021-01-07 09:35:07 -0500

    He creado el archivo dbCursos_schema

---
+++ dbCursos_schema.sql
@@ -0,0 +1,9 @@
+-- Crear la base de datos SAKILA
+CREATE DATABASE dbCursos
+DEFAULT CHARACTER SET utf8;
+
+-- Poner en uso la base de datos SAKILA
+USE dbCursos;
+
+-- Ver cual es la base de datos en uso
+SELECT DATABASE();
